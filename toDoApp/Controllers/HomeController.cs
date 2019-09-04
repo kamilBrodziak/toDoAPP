f3b163d401cb2420073b02e304e20997fe5863a5
kamilBrodziak <kamil98brodziak@gmail.com>
2019-09-05 01:22:44 +0200

    Update nav in layoutStartPage, add references to sites and activeNav class functionality

--- toDoApp/Controllers/HomeController.cs
+++ toDoApp/Controllers/HomeController.cs
@@ -16,6 +16,14 @@ namespace toDoApp.Controllers {
             return View();
         }
 
+        public IActionResult Login() {
+            return View();
+        }
+
+        public IActionResult Register() {
+            return View();
+        }
+
         public IActionResult Privacy() {
             return View();
         }
